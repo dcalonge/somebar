602abb4a49e7eee238cc8f45f57bfbc8fa8f58f3
dcalonge <dcalonge01@gmail.com>
2024-08-31 20:20:56 +0200

    update

--- src/config.hpp
+++ src/config.hpp
@@ -12,7 +12,7 @@ constexpr int paddingY = 3;
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
 constexpr const char* font = "JetBrains Mono Nerd Font 10";
 
-constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x22, 0x22, 0x22)};
+constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x14, 0x14, 0x14)};
 constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x00, 0x55, 0x77)};
 constexpr const char* termcmd[] = {"foot", nullptr};
 
